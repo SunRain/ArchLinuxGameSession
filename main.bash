25b7b9ef851b638f198e82c46636d68f0df72918
SunRain <41245110@qq.com>
2023-12-12 17:01:51 +0800

    wip

--- main.bash
+++ main.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-set -e
-set -x
+#debug
+set -e #如果命令执行失败，则立即退出 Shell。
+set -u #如果使用未定义的变量，则显示错误信息并退出 Shell。
+set -x #显示每个命令执行的详细信息。
+#set -v #显示 Shell 中每个命令执行之前的参数和输入。
 
 source ${PWD}/library.bash
 
@@ -19,3 +22,23 @@ fi
 USER_NAME=$1;
 
 echo "user name "${USER_NAME}
+
+#############################
+#  set archlinuxcn repo and install basic aur files
+#############################
+
+cat << EOF >> /etc/pacman.conf
+\n[archlinuxcn]
+SigLevel = Never
+Server = https://repo.archlinuxcn.org/\$arch
+\n
+EOF
+
+pacman -Syy
+pacman -S --noconfirm archlinuxcn-keyring paru
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+
+
